f71e058ad3f0a22447c7288dfba7f5f51116c15e
awal-cendana <108106499+cendana-awal1@users.noreply.github.com>
2022-11-06 05:03:19 +0000

    add route

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import route from "./routes/index.js"
+
+
+const URI =
+  "mongodb+srv://awalmakka:awalmakka@cluster0.v86wgfg.mongodb.net/?retryWrites=true&w=majority/express-mongo";
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Database"));
+
+
 const app = express();
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-    });
+app.use('/nilai',route);
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-    }
-);
+  console.log(`Listening on port ${port}`);
+});
