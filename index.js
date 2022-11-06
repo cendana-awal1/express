eb010ca8d69535f90dd7afff1290b6e5fdebc90c
awal-cendana <108106499+cendana-awal1@users.noreply.github.com>
2022-11-06 03:13:33 +0000

    initial app

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-issadasd
+import express from "express";
+const app = express();
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+    });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+    }
+);
