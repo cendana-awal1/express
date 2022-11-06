8fdb650a4d006ca11021d3c6fd28dc3ce76e3028
awal-cendana <108106499+cendana-awal1@users.noreply.github.com>
2022-11-06 05:20:22 +0000

    4

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@ const router = express.Router();
 router.get("/", (req, res) => {
     res.send("Hello World!");
   });
+router.post("/", (req, res) => {
+    res.send("welcome to post");
+  });
 
 export default router;
 
